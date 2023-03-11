11697b9611de9941a8ea9399c4a344efbfa7e2ab
Öykü Kaya <oykuky001@gmail.com>
2023-03-11 21:12:03 +0300

    Data types

--- Day 1/DataTypes.js
+++ Day 1/DataTypes.js
@@ -5,6 +5,7 @@
 *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
 *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
 *
+*
 *	Parameter(s):
 *   secondInteger - The string representation of an integer.
 *   secondDecimal - The string representation of a floating-point number.
