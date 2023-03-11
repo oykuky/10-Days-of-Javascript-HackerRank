fb9d1bf4065681d6d7dbf9e70fee753461638ea4
Öykü Kaya <oykuky001@gmail.com>
2023-03-11 21:11:05 +0300

    Hello world

--- Day 0/Hello, World!.js
+++ Day 0/Hello, World!.js
@@ -3,6 +3,7 @@
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
 *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
 *
+*
 *	Parameter:
 *   parameterVariable - A string of text.
 **/
